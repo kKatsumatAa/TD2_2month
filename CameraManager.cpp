3c0328a9ca367f4eab67b7f7269f08a462bef982
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2023-01-21 13:40:41 +0900

    回転時のカメラをイージング

--- CameraManager.cpp
+++ CameraManager.cpp
@@ -22,13 +22,25 @@ CameraManager::~CameraManager()
 	goalEffectCamera.reset();
 }
 
+void CameraManager::ChangeUsingCameraState(UsingCameraState* state)
+{
+	delete this->state;
+	this->state = state;
+	this->state->SetCameraM(this);
+}
+
 void CameraManager::Initialize()
 {
-	
+	lerpCount = 0;
+	lerpCountMax = 0;
+
+	ChangeUsingCameraState(new UsingCameraNormalState);
 }
 
 void CameraManager::Update()
 {
+	state->Update();
+
 	usingCamera->Update();
 	//ゲーム中メインで使うカメラ
 	gameMainCamera->Update();
@@ -36,4 +48,50 @@ void CameraManager::Update()
 	gameTurnCamera->Update();
 	//ゴール演出時に使うカメラ
 	goalEffectCamera->Update();
+}
+
+void CameraManager::BegineLerpUsingCamera(Vec3 startEye, Vec3 endEye, Vec3 startTarget, Vec3 endTarget, Vec3 startUp, Vec3 endUp,int time)
+{
+	this->startEye = startEye;
+	this->endEye = endEye;
+	this->startTarget = endTarget;
+	this->endTarget = endTarget;
+	this->startUp = startUp;
+	this->endUp = endUp;
+	this->lerpCountMax = time;
+	this->lerpCount = 0;
+
+	ChangeUsingCameraState(new UsingCameraLerpMoveState);
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+void UsingCameraState::SetCameraM(CameraManager* cameraM)
+{
+	this->cameraM = cameraM;
+}
+
+
+//-------------------------------------------------------------------------------------------------------
+void UsingCameraNormalState::Update()
+{
+
+}
+
+//-------------------------------------------------------------------------------------------------------
+void UsingCameraLerpMoveState::Update()
+{
+	float t = (float)cameraM->lerpCount / (float)cameraM->lerpCountMax;
+
+	cameraM->lerpCount++;
+
+	cameraM->usingCamera->SetEye(LerpVec3(cameraM->startEye, cameraM->endEye, EaseOut(t)));
+	cameraM->usingCamera->SetUp(LerpVec3(cameraM->startUp, cameraM->endUp, EaseOut(t)));
+	cameraM->usingCamera->SetTarget(LerpVec3(cameraM->startTarget, cameraM->endTarget, EaseOut(t)));
+
+
+	if (cameraM->lerpCount >= cameraM->lerpCountMax)
+	{
+		cameraM->ChangeUsingCameraState(new UsingCameraNormalState);
+	}
 }
--- CameraManager.h
+++ CameraManager.h
@@ -1,10 +1,40 @@
 #pragma once
 #include"Camera.h"
 
-class CameraManager
+class CameraManager;
+
+class UsingCameraState
+{
+protected:
+	CameraManager* cameraM = nullptr;
+
+public:
+	void SetCameraM(CameraManager* cameraM);
+	virtual void Update() = 0;
+};
+
+class UsingCameraNormalState : public UsingCameraState
 {
 private:
 
+
+public:
+	void Update() override;
+};
+
+class UsingCameraLerpMoveState : public UsingCameraState
+{
+private:
+
+public:
+	void Update() override;
+};
+
+
+class CameraManager
+{
+private:
+	UsingCameraState* state = nullptr;
 
 public:
 	//ここにいろんなカメラのポインタを入れて、描画時に使う
@@ -16,13 +46,21 @@ public:
 	//ゴール演出時に使うカメラ
 	std::unique_ptr<Camera> goalEffectCamera;
 
+	//lerp用
+	int lerpCount = 0;
+	int lerpCountMax = 0;
+	Vec3 startEye, endEye, startTarget, endTarget, startUp, endUp;
 
 public://関数
 	CameraManager();
 	~CameraManager();
 
+	void ChangeUsingCameraState(UsingCameraState* state);
+
 	void Initialize();
 
 	void Update();
 
+	//移動の線形補完を始める
+	void BegineLerpUsingCamera(Vec3 startEye, Vec3 endEye, Vec3 startTarget, Vec3 endTarget, Vec3 startUp, Vec3 endUp, int time);
 };
