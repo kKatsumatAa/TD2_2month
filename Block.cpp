0e4d43950e31cc1a00397ed89956097e888c6cf2
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2023-01-25 16:05:31 +0900

    ボタンの下にブロックを表示

--- Block.cpp
+++ Block.cpp
@@ -74,15 +74,28 @@ void Block::Draw(Camera* camera, UINT64* texhandle, int form, Action action, boo
 		color = { 0.5f,0.5f,0.85f,0.95f };
 
 		if(form == Form::BLOCK) { draw[0].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &normal_[0], color); }
-		if(form == Form::BUTTON) { draw[1].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &button_[0], color); }
+		if(form == Form::BUTTON) { draw[6].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &normal_[0], color); }
+		if(form == Form::BUTTON) 
+		{ 
+			worldTransform_.trans.y = 1.2;
+			draw[1].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &button_[0], color); 
+		}
+
 	}
 	else
 	{
 		color = { 0.1f,0.1f,0.1f,0.95f };
 
 		if(form == Form::BLOCK) { draw[0].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &normal_[0], color); }
-		if(form == Form::BUTTON) { draw[1].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &button_[0], color); }
+		if(form == Form::BUTTON) { draw[6].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &normal_[0], color); }
+		if(form == Form::BUTTON) 
+		{ 
+			worldTransform_.trans.y = 1.2;
+			draw[1].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &button_[0], color); 
+		}
+
 	}
+
 
 
 	/*if (form == Form::BLOCK) { draw[0].DrawModel(&worldTransform_, &camera->viewMat, &camera->projectionMat, &normal_[0], color); }
